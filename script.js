533d3631d8e9ded9d167b85fa9f61a38093e2b6e
nagendradevara <nagendradevara4@gmail.com>
2019-11-21 18:55:49 +0530

    commit todays work

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-function openMenu(){
-    alert("close");
+function openMenu(e){
+    
+let menu = document.getElementsByClassName('navbar-flex')
+for(let i=0;i<menu.length;i++){
+    menu[i].style.display="block";
+}
+
+// if(menu.style.display == "grid"){
+//     menu.style.display ="none";
+// }else{
+//     menu.style.display ="grid";
+// }
+
+}
+function createPortal(e){
+    console.log(e);
+    
+      let favDialog = document.getElementById('favDialog');
+      if (typeof favDialog.showModal === "function") {
+              favDialog.showModal();
+            } else {
+              alert("The dialog API is not supported by this browser");
+            }
 }
